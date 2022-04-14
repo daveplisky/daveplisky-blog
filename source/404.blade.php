2978799abd9efe658608f25199ef3b11f0a15f1b
daveplisky <82537138+daveplisky@users.noreply.github.com>
2022-04-14 15:06:12 -0400

    Update 404.blade.php

--- source/404.blade.php
+++ source/404.blade.php
@@ -4,7 +4,7 @@
     <div class="flex flex-col items-center text-gray-700 mt-32">
         <h1 class="text-6xl font-light leading-none mb-2">404</h1>
 
-        <h2 class="text-3xl">Page not found.</h2>
+        <h2 class="text-3xl">Whoops! We looked everywhere for that page, but we couldn't find it. ANYWHERE!</h2>
 
         <hr class="block w-full max-w-sm mx-auto border my-8">
 
